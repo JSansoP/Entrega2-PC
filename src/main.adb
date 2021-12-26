12f9c1eddd52a11a17dfe915df8398da73b53604
JSansoP <61268003+JSansoP@users.noreply.github.com>
2021-12-26 20:39:33 +0100

    Intent de fer delays aleatoris

--- src/main.adb
+++ src/main.adb
@@ -4,12 +4,17 @@ use Text_IO;
 with def_monitornordsud;
 use def_monitorNordSud;
 
+with ada.numerics.discrete_random;
+
 procedure Main is
+   type randRange is range 1..10;
+
 BABUINS : constant integer := 10; --N/2 babuins a cada costat
    MAX_COUNT : constant integer := 3; --Nombre de vegades que cada babui crusa
-
    monitor : NSMonitor;
-
+package Rand_Int is new ada.Numerics.Discrete_Random(randRange);
+   use Rand_Int;
+      gen: Generator;
    task type Babui is
       entry Start(Idx: in integer);
    end Babui;
@@ -18,6 +23,8 @@ BABUINS : constant integer := 10; --N/2 babuins a cada costat
       Id: integer;
       tipus : Character;
       nVegades : integer := 0;
+      rand : Generator;
+      aleatori: randRange;
    begin
       accept Start(Idx : in integer) do
          Id := Idx;
@@ -28,31 +35,38 @@ BABUINS : constant integer := 10; --N/2 babuins a cada costat
          end if;
 
       end Start;
+      reset(rand);
       Put_Line("Babui" & Id'img & " s'aixeca. Vol anar cap al "&tipus);
       for i in 0..MAX_COUNT loop
          if tipus = 'N' then
-            delay 1.0;
+            aleatori := random(gen);
+            delay aleatori'Image;
             monitor.nordLock;
             nVegades := nVegades + 1;
             Put_Line("Babui "& Id'img & " esta cruçant cap al " & tipus);
             Put_Line("Hi ha "& monitor.getNord'Img & " babuins a la corda direcció " & tipus);
-            delay 2.0;
+            aleatori := random(gen);
+            delay aleatori;
             monitor.nordUnlock;
             Put_Line("Babui "& Id'img & " baixa de la corda");
             Put_Line("Hi ha "& monitor.getNord'Img & " babuins a la corda direcció " & tipus);
-            delay 2.0;
+            aleatori := random(gen);
+            delay aleatori;
             Put_Line("Babui "& Id'img & " fa la volta.");
          else
-            delay 1.0;
+            aleatori := random(gen);
+            delay aleatori;
             monitor.sudLock;
             nVegades := nVegades + 1;
             Put_Line("Babui "& Id'img & " esta cruçant cap al " & tipus);
             Put_Line("Hi ha "& monitor.getSud'Img & " babuins a la corda direcció " & tipus);
-            delay 2.0;
+            aleatori := random(gen);
+            delay aleatori;
             monitor.sudUnlock;
             Put_Line("Babui "& Id'img & " baixa de la corda");
             Put_Line("Hi ha "& monitor.getSud'Img & " babuins a la corda direcció " & tipus);
-            delay 2.0;
+            aleatori := random(gen);
+            delay aleatori;
             Put_Line("Babui "& Id'img & " fa la volta.");
          end if;
 
@@ -60,11 +74,8 @@ BABUINS : constant integer := 10; --N/2 babuins a cada costat
       end Babui;
 
 type arraybabuins is array (1..BABUINS) of Babui;
-arrBabuins : arraybabuins;
-
 begin
    --  Insert code here.
-
 
 
    for Idx in 1..BABUINS loop
